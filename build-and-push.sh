49de3ea8e76d37d8b092aff1011d4bbe79e07827
Magnus Persson <magnus.persson@fortnox.se>
2024-11-12 09:27:48 +0000

    Disabled publishing to quay.io while outside of FortnoxAB

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,9 +2,12 @@
 
 export $(.github/version_extractor.sh Dockerfile)
 
-docker build . -t quay.io/fortnox/caddy-security-container:latest
+# Build and push to quay.io/fortnox/caddy-security-container disabled
 
-docker tag quay.io/fortnox/caddy-security-container:latest quay.io/fortnox/caddy-security-container:$CADDY_MAJOR
-docker tag quay.io/fortnox/caddy-security-container:latest quay.io/fortnox/caddy-security-container:$CADDY_MAJOR.$CADDY_MINOR
-docker tag quay.io/fortnox/caddy-security-container:latest quay.io/fortnox/caddy-security-container:$CADDY_MAJOR.$CADDY_MINOR.$CADDY_PATCH
-docker image push --all-tags quay.io/fortnox/caddy-security-container
+#docker build . -t quay.io/fortnox/caddy-security-container:latest
+docker build .
+
+#docker tag quay.io/fortnox/caddy-security-container:latest quay.io/fortnox/caddy-security-container:$CADDY_MAJOR
+#docker tag quay.io/fortnox/caddy-security-container:latest quay.io/fortnox/caddy-security-container:$CADDY_MAJOR.$CADDY_MINOR
+#docker tag quay.io/fortnox/caddy-security-container:latest quay.io/fortnox/caddy-security-container:$CADDY_MAJOR.$CADDY_MINOR.$CADDY_PATCH
+#docker image push --all-tags quay.io/fortnox/caddy-security-container
